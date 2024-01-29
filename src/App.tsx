e9e2748f6917b77a25cc80bc8825891504b80726
walteraandrade <walteraandrade@gmail.com>
2024-01-29 14:08:33 -0300

    added suggestion card

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Suspense } from 'react';
+import './App.css';
+import { Audio } from 'react-loader-spinner';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<div className="bg-slate-700">
+			<div className="container h-[100vh] m-auto py-[120px] ">
+				<h1 className="text-4xl text-white font-bold">Sugestões</h1>
+				<Suspense fallback={<Audio />}>
+					<SuggestionCard />
+				</Suspense>
+			</div>
+		</div>
+	);
 }
 
-export default App
+export default App;
+
+function SuggestionCard() {
+	interface Data {
+		Suggestions: any;
+	}
+
+	return (
+		<>
+			{/* {data?.Suggestions.map((suggestion: any) => (
+				<div
+					key={suggestion.id}
+					className="bg-white rounded-lg shadow-lg p-4 mt-4 flex flex-col items-center"
+				>
+					<a href={suggestion.link} target="_blank" className="cursor-pointer">
+						<h2 className="text-xl font-bold">{suggestion.title}</h2>
+						<p className="text-gray-500">{suggestion.author}</p>
+						<img
+							src={suggestion.image}
+							alt={suggestion.title}
+							className="w-full max-h-[500px] max-w-[500px] mt-4"
+						/>
+						<div className="mt-12" />
+						<p className="text-gray-500">
+							Sugestão de: {suggestion.suggested_by}
+						</p>
+					</a>
+				</div>
+			))} */}
+		</>
+	);
+}
